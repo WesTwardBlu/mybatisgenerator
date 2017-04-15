6f773350aba0b637e1a4ea4c673b58d0ba746063
935105579@qq.com <935105579@qq.com>
2017-04-15 11:47:35 +0800

    add some files

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # mybatisgenerator
+**这是一个介绍如何使用mybatis generator工具的demo。里面包括相关的配置**  
+>mybatis generator简称MBG  
+[官方介绍](http://www.mybatis.org/generator/)  
+*核心jar包：mybatis-generator-core-1.3.5.jar*  
+*mysql:5.0.8*  
+*jdk:1.7*  
+*javaEE:6*  
+  
+[启动MBG的两种方式](http://www.cnblogs.com/westward/p/6711135.html)
---
+++ src/com/westward/test/GenMain.java
@@ -0,0 +1,50 @@
+package com.westward.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+public class GenMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		List<String> warnings= new ArrayList<String>();
+		boolean overwrite= true;
+		String genCfg= "G:/workspace10/mybatisgenerator/src/mbgConfiguration.xml";
+		File configFile= new File(genCfg);
+		ConfigurationParser cp= new ConfigurationParser(warnings);
+		Configuration config= null;
+		try {
+			config= cp.parseConfiguration(configFile);
+			DefaultShellCallback callback= new DefaultShellCallback(overwrite);
+			MyBatisGenerator myBatisGenerator= null;
+			myBatisGenerator= new MyBatisGenerator(config, callback, warnings);
+			myBatisGenerator.generate(null);
+			
+			System.out.println(warnings);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (XMLParserException e) {
+			e.printStackTrace();
+		} catch (InvalidConfigurationException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
